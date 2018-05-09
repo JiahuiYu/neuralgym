4b8f7c6ae4a9fdbb7bf36fae19861dd5fa41b514
JiahuiYu <hi@jhyu.me>
2018-05-09 02:07:29 +0000

    add gan hinge loss

--- neuralgym/ops/gan_ops.py
+++ neuralgym/ops/gan_ops.py
@@ -47,6 +47,23 @@ def gan_ls_loss(pos, neg, value=1., name='gan_ls_loss'):
     return g_loss, d_loss
 
 
+def gan_hinge_loss(pos, neg, value=1., name='gan_hinge_loss'):
+    """
+    gan with hinge loss:
+    https://github.com/pfnet-research/sngan_projection/blob/c26cedf7384c9776bcbe5764cb5ca5376e762007/updater.py
+    """
+    with tf.variable_scope(name):
+        hinge_pos = tf.reduce_mean(tf.nn.relu(1-pos))
+        hinge_neg = tf.reduce_mean(tf.nn.relu(1+neg))
+        scalar_summary('pos_hinge_avg', hinge_pos)
+        scalar_summary('neg_hinge_avg', hinge_neg)
+        d_loss = tf.add(.5 * hinge_pos, .5 * hinge_neg)
+        g_loss = -tf.reduce_mean(neg)
+        scalar_summary('d_loss', d_loss)
+        scalar_summary('g_loss', g_loss)
+    return g_loss, d_loss
+
+
 def gan_wgan_loss(pos, neg, name='gan_wgan_loss'):
     """
     wgan loss function for GANs.
